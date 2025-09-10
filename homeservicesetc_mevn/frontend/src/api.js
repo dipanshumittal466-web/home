a664baf02dc14294041fa4f520b857feaff1f480
Dipanshu MITTAL <dipanshumittal466@gmail.com>
2025-09-10 12:32:27 +0530

    Update api.js

--- homeservicesetc_mevn/frontend/src/api.js
+++ homeservicesetc_mevn/frontend/src/api.js
@@ -1,9 +1,10 @@
+// frontend/src/api.js
 import axios from "axios";
 
-// ✅ Base URL automatic select (local OR render)
+// Base URL automatic select (local OR production)
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://home-dchr.onrender.com/api",
-  withCredentials: true
+  baseURL: import.meta.env.VITE_API_URL || "https://api.homeservicesetc.com/api",
+  withCredentials: true,
 });
 
 export default API;
