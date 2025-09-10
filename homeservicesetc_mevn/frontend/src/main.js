2c1f94885c6e89944a7c1007c56786aeb1a26647
Dipanshu MITTAL <dipanshumittal466@gmail.com>
2025-09-10 09:23:11 +0530

    Update main.js

--- homeservicesetc_mevn/frontend/src/main.js
+++ homeservicesetc_mevn/frontend/src/main.js
@@ -1,7 +1,8 @@
 const app = document.getElementById('app');
 
 async function load(){
-  const cats = await fetch('/api/categories').then(r=>r.json());
+  const cats = await fetch(`${import.meta.env.VITE_API_URL}/categories`)
+  .then(r => r.json());
   let totalSubs = cats.reduce((acc,c)=> acc + (c.children?c.children.length:0), 0);
   app.innerHTML = `<h1>HomeServicesEtc</h1>
   <p><b>11</b> main categories (icons) + <b>${totalSubs}</b> subcategories = <b>${11+totalSubs}</b> total.</p>
