d31d304e3584fcf9791cc9b09841e71c45a33b5b
Dipanshu MITTAL <dipanshumittal466@gmail.com>
2025-09-10 09:46:42 +0530

    Update vite.config.js

--- homeservicesetc_mevn/frontend/vite.config.js
+++ homeservicesetc_mevn/frontend/vite.config.js
@@ -1,4 +1,16 @@
-export default {
-  server: { port: 5173, proxy: { '/api': 'http://localhost:8080' } },
-  build: { outDir: 'dist' }
-}
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': 'http://localhost:8080'
+    }
+  },
+  build: {
+    outDir: 'dist'
+  }
+})
