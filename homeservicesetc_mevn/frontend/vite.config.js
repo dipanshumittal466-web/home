adc8b92d9057136354db8224bba28dadd7cb5037
Dipanshu MITTAL <dipanshumittal466@gmail.com>
2025-09-10 10:15:12 +0530

    Update vite.config.js

--- homeservicesetc_mevn/frontend/vite.config.js
+++ homeservicesetc_mevn/frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
@@ -12,5 +11,6 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist'
-  }
+  },
+  base: './'   
 })
